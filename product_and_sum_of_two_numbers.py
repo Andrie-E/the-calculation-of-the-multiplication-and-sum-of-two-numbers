b5379a1df20d083a1824146383aab36b43451027
Andrie-E <stpaul.martinla@gmail.com>
2024-01-26 14:02:36 +0800

    pseudocode and values

---
+++ product_and_sum_of_two_numbers.py
@@ -0,0 +1,17 @@
+# Given two integer numbers, return their product only if the product is equal to or lower than 1000. Otherwise, return their sum.
+
+# Pseudocode
+
+# Indicate the values
+number1 = 20
+number2 = 30
+number3 = 40
+number4 = 30
+
+# Check if the values for given 1 is less than or equal to 1000
+# Return if the condition is true
+# If the values are over 1000, sum it  
+# Check if the values for given 2 is less than or equal to 1000  
+# Return if the condition is true
+# If the values are over 1000, sum it
+# Print the result
